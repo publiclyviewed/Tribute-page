5c89c3a3e49857b80aeaefe79af0b9426b3305d0
Dave <publiclyviewed@gmail.com>
2025-01-04 11:01:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function playAudio() {
+    document.getElementById('audio').play();
+}
+
+function pauseAudio() {
+    document.getElementById('audio').pause();
+}
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+  }
